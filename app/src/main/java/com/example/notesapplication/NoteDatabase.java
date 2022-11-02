beb6249928753e1afc6fb4f90c2c91d3fac8ddcb
amushawwir <mushawwir123@gmail.com>
2022-11-02 22:27:56 +0700

    UTS Mobile

---
+++ app/src/main/java/com/example/notesapplication/NoteDatabase.java
@@ -0,0 +1,40 @@
+package com.example.notesapplication;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.arch.persistence.room.TypeConverters;
+import android.content.Context;
+
+import com.example.notesapplication.NoteDao;
+import com.example.notesapplication.Note;
+import com.example.notesapplication.Constants;
+import com.example.notesapplication.DateRoomConverter;
+
+@Database(entities = { Note.class }, version = 1)
+@TypeConverters({DateRoomConverter.class})
+public abstract class NoteDatabase extends RoomDatabase {
+
+    public abstract NoteDao getNoteDao();
+
+
+    private static NoteDatabase noteDB;
+
+    // synchronized is use to avoid concurrent access in multithred environment
+    public static /*synchronized*/ NoteDatabase getInstance(Context context) {
+        if (null == noteDB) {
+            noteDB = buildDatabaseInstance(context);
+        }
+        return noteDB;
+    }
+
+    private static NoteDatabase buildDatabaseInstance(Context context) {
+        return Room.databaseBuilder(context,
+                NoteDatabase.class,
+                Constants.DB_NAME).allowMainThreadQueries().build();
+    }
+
+    public  void cleanUp(){
+        noteDB = null;
+    }
+}
